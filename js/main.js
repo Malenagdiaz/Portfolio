77f5e544df502da54c27fbfa4b28cea42a8f9cf0
Malena Gerez Diaz <mgerezdiaz@gmail.com>
2024-07-10 15:43:11 -0300

    Añado los primeros estilos para el header y la intro

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const descripcion = document.querySelector(".descripcion");
+const descripcionTextos = [
+  "Desarrolladora web",
+  "Apasionada por la tecnología",
+  "Especializada en la creación de sitios web",
+];
+
+let textoActual = 0;
+let texto = "";
+
+function borrarTexto() {
+  const textoArray = texto.split("");
+  let i = textoArray.length - 1;
+
+  const borrarInterval = setInterval(() => {
+    texto = textoArray.slice(0, i).join("");
+    descripcion.textContent = texto;
+    i--;
+
+    if (i < 0) {
+      clearInterval(borrarInterval);
+      texto = "";
+      textoActual = (textoActual + 1) % descripcionTextos.length;
+      setTimeout(tipeo, 1000);
+    }
+  }, 50);
+}
+
+function tipeo() {
+  const textoArray = descripcionTextos[textoActual].split("");
+  let i = 0;
+
+  const tipoInterval = setInterval(() => {
+    texto += textoArray[i];
+    descripcion.textContent = texto;
+    i++;
+
+    if (i >= textoArray.length) {
+      clearInterval(tipoInterval);
+      setTimeout(borrarTexto, 2000);
+    }
+  }, 100);
+}
+
+tipeo();
